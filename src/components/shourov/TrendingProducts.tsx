93320ea0d21ba28546048e852033854dd2b030ac
shourov223 <shourav4316@gmail.com>
2026-04-22 00:55:31 +0600

    font family added

---
+++ src/components/shourov/TrendingProducts.tsx
@@ -0,0 +1,11 @@
+const TrendingProducts = () => {
+  return (
+    <section>
+      <div className="section_wrapper bg-[#FFF9EB] pt-[151px] pb-[75px]">
+        <div className="container"></div>
+      </div>
+    </section>
+  );
+};
+
+export default TrendingProducts;
