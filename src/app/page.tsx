609e45161632b1053f789132ecc79b0ae9529b21
Md-Gajjali <hellogajjali@gmail.com>
2026-04-22 03:04:34 +0600

    faaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import Image from "next/image";
-import BestProduct from "../components/emam/BestProduct";
-import Feedbacks from "../components/emam/Feedbacks";
 
 export default function Home() {
     return (
         <>
-            {/* // <div className="container">
-            //     <h1 className="text-6xl text-red-600">HEllO WORLD</h1>
-            // </div> */}
-            <BestProduct />
-            <Feedbacks />
+            <div className="container">
+                <h1 className="text-6xl text-red-600">Dui ta section ke call kren </h1>
+            </div>
         </>
     );
 }
