83f53cd71b59e355205262fe21e944b11c749d66
Md-Gajjali <hellogajjali@gmail.com>
2026-04-22 02:51:27 +0600

    update

--- src/components/emam/Feedbacks.tsx
+++ src/components/emam/Feedbacks.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image"
 import w1 from '@/src/assets/w1.png'
+import star from '@/src/assets/icon/Star.png'
 
 const Feedbacks = () => {
 
   return (
-   <section>
+   <section className="pb-[150px]">
     <div className="container">
         <div className="text-center flex items-center justify-center">
           <div>
@@ -13,13 +14,24 @@ const Feedbacks = () => {
           </div>
         </div>
 
-        <div className="flex ">
-            <div className="bg-[#D9D9D9] flex items-center justify-center text-center">
+        <div className="flex mt-[112px] ">
+            <div className="bg-[#D9D9D9] flex items-center justify-center text-center rounded-tr-[50px] rounded-bl-[50px] rounded-bl-[50px]">
                 <div className="py-[60px] px-4.5">
                     <div>
-                        <Image src={w1} alt="image" />
-                        <h3 className="font-semibold text-[20px]">Emma Charlotte</h3>
+                        <Image src={w1} alt="image" className="mx-auto"/>
+                        <h3 className="font-semibold text-[20px] mt-2.5">Emma Charlotte</h3>
+                        <p>Singer</p>
                     </div>
+                    <div className="flex items-center justify-center  mt-11">
+                        <div className="flex gap-2.5">
+                            <Image src={star} alt="image"/>
+                            <Image src={star} alt="image"/>
+                            <Image src={star} alt="image"/>
+                            <Image src={star} alt="image"/>
+                            <Image src={star} alt="image"/>
+                        </div>
+                    </div>
+                        <p className="w-[382px] text-[#707070] mt-2.5 font-normal text-[20px]">There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain</p>
                 </div>
             </div>
         </div>
