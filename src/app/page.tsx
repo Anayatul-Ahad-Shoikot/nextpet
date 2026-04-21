79775e734889b54fb9cb46813724c24828f74fea
Anayatul Ahad Shoikot <shoikotofficial@gmail.com>
2026-04-22 00:16:17 +0600

    project basic setup done

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="container">
+            <h1 className="text-6xl text-red-600">HEllO WORLD</h1>
+        </div>
+    );
+}
