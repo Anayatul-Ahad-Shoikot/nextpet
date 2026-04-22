3c68ce725f4d647e56971d312834af9a72685ca5
shourov223 <shourav4316@gmail.com>
2026-04-22 12:10:09 +0600

    Shourov : Trending product section and product card created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import TrendingProducts from "../components/shourov/TrendingProducts";
 
 export default function Home() {
   return (
     <main>
-      <TrendingProducts />
+      
     </main>
   );
 }
