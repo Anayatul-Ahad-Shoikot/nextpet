e1073dd2ef1904e32669f363571cd6341746761c
Anayatul Ahad Shoikot <shoikotofficial@gmail.com>
2026-04-22 12:23:49 +0600

    Ahad: page.tsx conflict resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "../components/ahad/HeroSection";
 import StatisticsSection from "../components/ahad/StatisticsSection";
 import BestProduct from "../components/emam/BestProduct";
+import Feedbacks from "../components/emam/Feedbacks";
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <HeroSection />
             <StatisticsSection />
             <BestProduct />
+            <Feedbacks />
         </>
     );
 }
