4689306433feee9a704abe262d05bc13c590b4bd
Alamin-Coding <md.alamin.coding@gmail.com>
2026-04-22 15:07:07 +0600

    pull to shurov barnch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 import NewArrivals from "../components/shourov/NewArrivals";
 import TrendingProducts from "../components/shourov/TrendingProducts";
 
-export default function Home() {
-  return (
-    <main>
-    </main>
-  );
-=======
 import HeroSection from "../components/ahad/HeroSection";
 import StatisticsSection from "../components/ahad/StatisticsSection";
 import BestProduct from "../components/emam/BestProduct";
@@ -15,14 +8,15 @@ import Features from "../components/munna/Features";
 import Feedbacks from "../components/emam/Feedbacks";
 
 export default function Home() {
-    return (
-        <>
-            <HeroSection />
-            <StatisticsSection />
-            <BestProduct />
-            <Feedbacks />
-            <Features />
-        </>
-    );
->>>>>>> 73c544c8cd9f49a18a9794d018ecf3e12a7bf4da
+	return (
+		<>
+			<HeroSection />
+			<StatisticsSection />
+			<BestProduct />
+			<Feedbacks />
+			<Features />
+			<TrendingProducts />
+			<NewArrivals />
+		</>
+	);
 }
