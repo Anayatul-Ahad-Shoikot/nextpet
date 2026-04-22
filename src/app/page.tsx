194627bdee207162bc1d3e81eea07952f6a4eb7b
Alamin-Coding <md.alamin.coding@gmail.com>
2026-04-22 15:28:44 +0600

    Sir: pech khulse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
 		<>
 			<HeroSection />
 			<StatisticsSection />
+			<TrendingProducts />
+			<NewArrivals />
 			<BestProduct />
 			<Feedbacks />
 			<Features />
-			<TrendingProducts />
-			<NewArrivals />
 		</>
 	);
 }
